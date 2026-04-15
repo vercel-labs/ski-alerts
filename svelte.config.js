2101e4a73bc459cbf9ae84a629d10546f6515b89
abser <vazquezsergio99@hotmail.com>
2026-04-15 12:52:41 -0600

    new fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 import adapter from '@sveltejs/adapter-vercel';
-
-/** @type {import('@sveltejs/kit').Config} */
+ 
 const config = {
-	kit: {
-		adapter: adapter({runtime: 'nodejs24.x'})
-	}
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs24.x'
+    })
+  }
 };
-
+ 
 export default config;
